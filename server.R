bbd44422486f74d5362120d6bbee7651c6b3f61d
Brandon Gavett <begavett@yahoo.com>
2014-04-04 02:49:16 -0600

    Reliable Change index
    Shiny app to calculate reliable change

---
+++ server.R
@@ -0,0 +1,81 @@
+library(shiny)
+
+# Define server logic required to plot various variables against mpg
+shinyServer(function(input, output) {
+  Yprime <- reactive({
+    if (input$RCI == "JT") {
+      return(round(input$EITS,2))
+    }
+    if (input$RCI == "Speer") { 
+      return(round(input$CGRM + input$TRR*(input$EITS - input$CGITM),2))
+    }
+    if (input$RCI == "Chelune") {
+      return(round(input$EITS + (input$CGRM - input$CGITM),2))
+    }
+    if (input$RCI == "McSweeny") {
+      b <- input$TRR * (input$CGRS/input$CGITS)
+      a <- input$CGRM - b*input$CGITM
+      return(round(b*input$EITS + a,2))
+    }
+    if (input$RCI == "Charter") {
+      return(round(input$CGRM + input$TRR * (input$EITS - input$CGITM),2))
+    }
+    if (input$RCI == "CH") {
+      b <- input$TRR * (input$CGRS/input$CGITS)
+      a <- input$CGRM - b*input$CGITM
+      return(round(b*input$EITS + a,2))
+    }
+    if (input$RCI == "Temkin") {
+      return(round(input$EITS + (input$CGRM - input$CGITM),2))
+    }
+    if (input$RCI == "Iverson") {
+      return(round(input$EITS + (input$CGRM - input$CGITM),2))
+    }
+    if (input$RCI == "Maassen") {
+      badj <- input$CGRS/input$CGITS
+      aadj <- input$CGRM - badj*input$CGITM
+      return(round(badj*input$EITS + aadj,2))
+    }})
+  
+  
+  SE <- reactive({
+    if (input$RCI == "JT") {
+      return(round(sqrt(2*input$CGITS*(1-input$TRR)),2))
+    }
+    if (input$RCI == "Speer") { 
+      return(round(sqrt(2*input$CGITS*(1-input$TRR)),2))
+    }
+    if (input$RCI == "Chelune") {
+      return(round(sqrt(2*input$CGITS*(1-input$TRR)),2))
+    }
+    if (input$RCI == "McSweeny") {
+      return(round(input$CGRS*sqrt(1-input$TRR^2),2))
+    }
+    if (input$RCI == "Charter") {
+      return(round(input$CGRS*sqrt(1-input$TRR^2),2))
+    }
+    if (input$RCI == "CH") {
+      SE4 <- sqrt(2*input$CGITS*(1-input$TRR))
+      return(round(SE4*sqrt(1+(1/input$CGSS)+(((input$EITS-input$CGITM)^2)/((input$CGITS^2)*(input$CGSS-1)))),2))
+    }
+    if (input$RCI == "Temkin") {
+      return(round(sqrt(input$CGITS^2 + input$CGRS^2 + 2*input$CGITS*input$CGRS*input$TRR),2))
+    }
+    if (input$RCI == "Iverson") {
+      return(round(sqrt((input$CGITS^2 + input$CGRS^2)*(1-input$TRR)),2))
+    }
+    if (input$RCI == "Maassen") {
+      return(round(sqrt((input$CGITS^2 + input$CGRS^2)*(1-input$TRR)),2))
+    }
+  })
+  LowerCI <- reactive({Yprime()+(qnorm((100-input$CI)/200)*SE())})
+  UpperCI <- reactive({Yprime()+(qnorm((100-input$CI)/200,lower.tail=FALSE)*SE())})
+  zscore <- reactive({(input$OT2S - Yprime())/SE()})
+
+  output$RCI <- renderText(input$RCI)
+  output$PT2S <- renderText(paste0("Predicted Time 2 Score: ", Yprime()))
+  output$SE <- renderText(paste0("Standard Error: ", SE()))
+  output$LowerCI <- renderText({paste0("Lower Band of ", as.numeric(input$CI), "% Interval: ", LowerCI())})
+  output$UpperCI <- renderText({paste0("Upper Band of ", as.numeric(input$CI), "% Interval: ", UpperCI())})
+  output$zscore <- renderText({paste0("z-score: ", zscore())})
+})
---
+++ ui.R
@@ -0,0 +1,36 @@
+library(shiny)
+
+shinyUI(pageWithSidebar(
+  # Application Title
+  headerPanel("Reliable Change Calculator"),
+  
+  #Sidebar with controls to enter hits, etc.
+  sidebarPanel(
+    selectInput("RCI","Reliable Change Model:",
+                choices=list("Jacobson & Truax (1991)" = "JT",
+                             "Speer (1992)" = "Speer",
+                             "Chelune et al. (1993)" = "Chelune",
+                             "McSweeny et al. (1993)" = "McSweeny",
+                             "Charter (1996)" = "Charter",
+                             "Crawford & Howell (1998)" = "CH",
+                             "Temkin et al. (1999)" = "Temkin",
+                             "Iverson et al. (2003)" = "Iverson",
+                             "Maassen et al. (2006)" = "Maassen"),multiple=FALSE),
+    numericInput("CI", "Confidence Interval (%):",95,min=0,max=100),
+    numericInput("EITS", "Examinee's Initial Test Score:",0),
+    numericInput("CGITM", "Control Group Initial Test Mean:",0),
+    numericInput("CGITS", "Control Group Initial Test SD:",0),
+    numericInput("CGRM", "Control Group Retest Mean:",0),
+    numericInput("CGRS", "Control Group Retest SD:",0),
+    numericInput("TRR", "Test-Retest Reliability:",0,min=0,max=1),
+    numericInput("CGSS", "Control Sample Size (N):",0,min=0),
+  numericInput("OT2S", "Observed Time 2 Score:",0),
+  submitButton("Calculate")),
+  
+  mainPanel(textOutput("PT2S"),
+            textOutput("SE"),
+            textOutput("LowerCI"),
+            textOutput("UpperCI"),
+            textOutput("zscore"))
+  
+))
